32d5127e4f012a2aff40c0116942e3a191edb90a
KangSeyun <dcp.k953@gmail.com>
2018-05-06 14:49:22 +0900

    feat(router): Router 입력
    Signed-off-by: KangSeyun <dcp.k953@gmail.com>

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const router = require('./router')
+
 const app = express();
 
 app.use(morgan(':method :url :status :response-time ms'));
-
-app.get('/', (req, res) => {
-    res.send('hi');
-});
+app.use('/api', router)
 
 app.listen(3000, () => {
     console.log('server is running .. port : 3000')
---
+++ app/controller/auth.js
@@ -0,0 +1,4 @@
+exports.test = async (req, res) => {
+    console.log("Hello World")
+    res.send(200, "Hello")  
+}
