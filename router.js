8b4bd8683a28017306446c82a8d2e6d02a049b58
ji <wldnjs5818@naver.com>
2018-05-20 15:57:43 +0900

    creating project API

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { test, findID, findPW, login, signUp, signUpMain, memberLeave } = require('./app/controller/auth')
+const { test, findID, findPW, login, signUp, signUpMain, memberLeave, proj, projCreate, projModify } = require('./app/controller/auth')
 
 router.get('/test', test);
 router.post('/findID', findID);
@@ -10,5 +10,10 @@ router.route('/join')
 .get(signUp)
 .post(signUpMain)
 .delete(memberLeave);
+
+router.route('/project')
+.get(proj)
+.post(projCreate)
+.put(projModify);
 
 module.exports = router
